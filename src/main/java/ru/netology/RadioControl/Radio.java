fdac3fcf07dc120f3de3a0fc1b5211d07c9a5803
Julia <avokdorog@mail.ru>
2023-08-14 15:35:09 +0300

    first commit

---
+++ src/main/java/ru/netology/RadioControl/Radio.java
@@ -0,0 +1,67 @@
+package ru.netology.RadioControl;
+
+public class Radio {
+    private final int minNumber = 0;
+    private final int maxNumber = 9;
+    private int currentNumber;       // номер текущей радиостанции
+
+    private final int minVolume = 0;
+    private final int maxVolume = 100;
+    private int currentVolume;       // громкость звука
+
+    public int getCurrentNumber() {
+        return currentNumber;
+    }
+
+    public void setCurrentNumber(int newCurrentNumber) {
+        if (newCurrentNumber < minNumber) {
+            return;
+        }
+        if (newCurrentNumber > maxNumber) {
+            return;
+        }
+        currentNumber = newCurrentNumber;
+    }
+
+    public void nextNumber() {
+        if (currentNumber < maxNumber) {
+            currentNumber = currentNumber + 1;
+        } else {
+            currentNumber = minNumber;
+        }
+    }
+
+    public void prevNumber() {
+        if (currentNumber > minNumber) {
+            currentNumber = currentNumber - 1;
+        } else {
+            currentNumber = maxNumber;
+        }
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int newCurrentVolume) {
+        if (newCurrentVolume < minVolume) {
+            newCurrentVolume = minVolume;
+        }
+        if (newCurrentVolume > maxVolume) {
+            newCurrentVolume = maxVolume;
+        }
+        currentVolume = newCurrentVolume;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < maxVolume) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+
+    public void reductionVolume() {
+        if (currentVolume > minVolume) {
+            currentVolume = currentVolume - 1;
+        }
+    }
+}
